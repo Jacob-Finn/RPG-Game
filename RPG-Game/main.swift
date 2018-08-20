6c7fdb0a1f2e8a8537d50a389208388bcc12941b
Jacob Finn <jacobfinn@macbook-pro-9.local>
2018-08-20 12:47:05 -0500

    Initial Commit

---
+++ RPG-Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RPG-Game
+//
+//  Created by Jacob Finn on 8/20/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
